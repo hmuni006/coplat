7b5e0389992ce328ab54713154ca37f7821bc7e8
Lorenzo Alexis Sanchez <lsanc104@fiu.edu>
2014-06-21 11:50:26 -0400

    About
    About page

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -8,5 +8,18 @@ $this->breadcrumbs=array(
 ?>
 <h1>About</h1>
 
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<p>Nowadays students that seek answers for school related questions usually use search engines as their main source of information;</br>
+    but in most cases this information is unreliable and incomplete. This project was envisioned to fulfill this need; to allow</br>
+    students to communicate with mentors that are experts in their field. In our mentoring platform students can post a question of</br>
+    any subject and it will be assigned to the most expertise domain mentor to get the most reliable and accurate answer. In senior</br>
+    project class, these platforms could be use for communication within team members and their mentors.</br></p>
+
+<lh>
+    <h4>Developers</h4>
+</lh>
+<lh>
+    <h5>Version 1.0</h5>
+</lh>
+<li> Lorenzo Alexis Sanchez</li>
+<li> Henry Daniel Muniz</li>
+<li> Steven Santiago Sanabria</li>
